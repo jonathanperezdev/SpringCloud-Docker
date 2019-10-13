2a99d445c78e93b70645f5ac390ec99bd8347fb3
jonathanperezdev <52081233+jonathanperezdev@users.noreply.github.com>
2019-10-13 13:55:57 -0500

    Develop (#1)
    * Added gitignore file and added EnableDiscoveryClient annotation
    
    * Modified README file to specify RELEASE1 content

--- postgresql/src/main/java/com/okta/developer/docker_microservices/postgresql/PostgresqlApplication.java
+++ postgresql/src/main/java/com/okta/developer/docker_microservices/postgresql/PostgresqlApplication.java
@@ -2,9 +2,11 @@ package com.okta.developer.docker_microservices.postgresql;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 @RefreshScope
 public class PostgresqlApplication {
 
